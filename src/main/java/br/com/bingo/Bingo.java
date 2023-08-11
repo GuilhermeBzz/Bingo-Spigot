2da7f754b93c4784904cdd88a296fcc5d0be1323
GuiBz <zeiglerguilherme@gmail.com>
2023-08-11 09:06:20 -0300

    V1.0

---
+++ src/main/java/br/com/bingo/Bingo.java
@@ -0,0 +1,17 @@
+package br.com.bingo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bingo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
