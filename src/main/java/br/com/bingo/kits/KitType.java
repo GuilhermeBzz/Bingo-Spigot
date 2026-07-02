cb639df4e58e638ae3283dc1d2ecf6ef7177f88b
GuilhermeBz <guilherme_zeigler@yahoo.com.br>
2026-07-02 17:17:41 -0300

    feat(cartografo): registra KitType.CARTOGRAPHER

--- src/main/java/br/com/bingo/kits/KitType.java
+++ src/main/java/br/com/bingo/kits/KitType.java
@@ -32,6 +32,7 @@ public enum KitType {
     SOULBOUND("Soulbound", Material.DRAGON_BREATH, "Abra seu Enderchest Portatil e use seu portal para voltar para a base!", new Soulbound()),
     PYRO("Pyro", Material.FLINT_AND_STEEL, "Use seu fogo para queimar seus inimigos e não tome dano para o fogo !", new Pyro()),
     ALCHEMIST("Alchemist", Material.EXPERIENCE_BOTTLE, "Crie itens com receitas especiais!", new Alchemist()),
+    CARTOGRAPHER("Cartographer", Material.MAP, "Localize estruturas do mundo e rastreie-as com sua bussola!", new Cartographer()),
     ;
 
 
